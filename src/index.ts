c40df12af10e5bf4fd8c5b3f6b5644ec08d455ca
CheemaMahesh <your_personal_email@example.com>
2025-10-08 20:50:59 +0530

    Intigrated env

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import express from "express";
 import { issueRouter } from "./routes/issue.routes";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+const port = process.env.PORT;
 app.use(express.json());
 
 app.use("/api/v1/issue", issueRouter);
 
-app.listen(3000, () => console.log("Server is up and running on port 3000"));
+app.listen(port, () => console.log(`Server is up and running on port ${port}`));
